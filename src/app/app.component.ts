d378399e2d9059b135208f189b5b2e2aa2ab65c6
tmic <noreply@eesc.europa.eu>
2020-07-14 16:00:03 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
-
+import { DOCUMENT } from '@angular/common';
 
 
 @Component({
@@ -9,28 +9,49 @@ import { Location } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit {
 	title = 'anilist';
   
-    constructor(private route: ActivatedRoute, private router: Router,     private location: Location) { 
+	constructor() { 
     }
   
-    ngOnInit() {
-		let token = null
-		
-		if(window.location.search !== "") {
-			let search = window.location.search
+	
+    ngAfterViewInit() {
+		// http://localhost:4200/#error=access_denied&state=
+		// http://localhost:4200/#access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImM4YjI5OWFhZDllNTFhZmViYjk4NGIwZDEzOTQ4MzA1MTNlMjc0YWU1YTMxOTE0MDYyOTMwMGNhMTlhY2FlNTYzOGRjM2FhYzJmMTYzY2E3In0.eyJhdWQiOiIzNzQ5IiwianRpIjoiYzhiMjk5YWFkOWU1MWFmZWJiOTg0YjBkMTM5NDgzMDUxM2UyNzRhZTVhMzE5MTQwNjI5MzAwY2ExOWFjYWU1NjM4ZGMzYWFjMmYxNjNjYTciLCJpYXQiOjE1OTQ3MzIzMDYsIm5iZiI6MTU5NDczMjMwNiwiZXhwIjoxNjI2MjY4MzA2LCJzdWIiOiI4MTk5MSIsInNjb3BlcyI6W119.hpYfulKrx5xnkQLjI1OIf8qoUmvkhoEcFyv6ZPgJTUsQJvbkdFO_TTmevp87EHnxukGHsvDyIqzy-40rHtvp_Wbdx0UgiRqtYS4r-KLAG1GrfHUI1GLRtXvLJfHTx3WFUIq9dRDLy20BgTE-418gyv7W5KEgB3Z59gGJKFow2RZ5WrnlAopxagIMegYBxUhtfigwdToHfJSvY_OWsOuhx2dyInwG07lZjF01D5WX-GFeSb_BrnQ8YUrJFMvJGqp1ct8skSLKkjttt_7o1ihHKMy03RludQNY4YvLJPE92ORk1RWAKp9MmRzYTVcA-l3rKXw4pmWuoDN1-ql304zy2bg2jiNuN9T4b6TWhYGRwGac4RniPVQVczUnTCLqeUJRu3bA93bW7QX0CvKM2SPOhXwcFENtAz8zJfbECCHgBRF1qrUhwQHV4retsBiz6mJjlP_lPKGhjSYIJythP6rInpTK1vfeI48tAXNAkFsxY3FHqo0WeR9wpV9OGwywDSsAi2ZznQCYH2Zyrzb_xq7VJgyIcRD9yWcsexhhGy0052zRtJGcs6Vwm94o9yAhiFAVqhJLd2t8Bi9P73fEIV0RpOB4s_XZ0ILby4ktpoDjSvGnQSBbsBYt3Rg5MivYSGWBrcpujz6uaMngk8JzscgLuOcAX2VLq7NOMv_U3aPBJlY&token_type=Bearer&expires_in=31536000
+
+
+		if(window.location.hash !== "") {
+			let hash = window.location.hash
 			
 			var queryDict = {}
-			search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
+			hash.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
 			
-			if(queryDict.token !== undefined) {
-				token = queryDict.token
+			let error = queryDict["error"]
+			let token = queryDict["access_token"]
+
+			if(error !== undefined) {
+				// redirect page erreur
+				return 
+			} else if(token !== undefined) {
+				localStorage.setItem("token", token)
+				// redirect library
 			}
-			
-			let href = window.location.href
-			this.location.replaceState('/', '')
 		}
+
+
+		let token = localStorage.getItem("token")
+
+		if(token) {
+			console.log(1)
+			// redirect library
+		} else {
+			console.log(2)
+			// redirect login
+			window.location.href = 'https://anilist.co/api/v2/oauth/authorize?client_id=3749&response_type=token'
+		}
+
+
 		
 	}
   
